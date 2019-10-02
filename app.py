e2fda4544eb7f20ce244a88b92dd41dc58a2cdd3
Philipp Allgeuer <philipp.allgeuer@zumtobelgroup.com>
2019-10-03 00:39:01 +0200

    :heavy_plus_sign: adding connexion

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from connexion import FlaskApp
 
-app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'hello'
+def get_variables():
+    pass
+
+app = FlaskApp(__name__)
+app.add_api('openapi.yaml')
+
+if __name__ == '__main__':
+    app.run()
