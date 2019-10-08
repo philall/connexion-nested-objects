10bf30832f1e365b284a022bfbd3c3a1e66e9335
Philipp Allgeuer <philipp.allgeuer@zumtobelgroup.com>
2019-10-08 17:26:44 +0200

    :recycle: test method

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask
+from flask import request
+from flask.views import View
 from connexion import FlaskApp
 
 
-def get_variables():
-    pass
+class SimpleTest(View):
+    def dispatch_request(self):
+        return 'foo'
 
-app = FlaskApp(__name__)
-app.add_api('openapi.yaml')
+get_test = SimpleTest.as_view('get_test')
 
 if __name__ == '__main__':
+    options = {"swagger_ui": True}
+
+    app = FlaskApp(__name__, options=options)
+    app.add_api('openapi.yaml')
     app.run()
